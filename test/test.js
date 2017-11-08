8a0ecec3591d30735e3ac0552c009348d06ea04a
fukuda1063 <ee57075@meiji.ac.jp>
2017-11-08 21:17:14 +0900

    test commit

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const myModule = require('../myModule.js');
 
 describe('myModule', function () {
     describe('greet', function () {
-        it('引数に応じて決まった文字列を返す',function () {
+        it('引数に応じて決まった文字列を返すこと',function () {
             assert.equal(myModule.greet('taro'),'Hello,taro');
         });
     });
